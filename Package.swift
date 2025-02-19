23e8e5952ae86786912ea659078e2270c690b172
h.cuong.le <h.cuong.le@samsung.com>
2025-02-19 18:24:03 +0700

    change to dynamic

--- Package.swift
+++ Package.swift
@@ -9,7 +9,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DEToolkit",
-            targets: ["DEToolkit"]),
+            type: .dynamic,
+            targets: ["DEToolkit"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
